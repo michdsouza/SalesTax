49d21874f737f71a43db9e3b2b6d5b88e0eda31d
AU Zoso <changingrainbows@gmail.com>
2011-07-09 23:07:07 -0300

    clean up classification

--- lib/classification.rb
+++ lib/classification.rb
@@ -1,26 +1,29 @@
 class Classification
 
   attr_reader :tax_rate  #it's the classification that governs tax rate. Needs to be attr_read/available for decoration?
-  
+    
+  TAX_EXEMPT_RATE = 0
+  DEFAULT_TAX_RATE = 10
+  IMPORT_TAX_RATE = 5
+
   def self.BOOK		#static objects
-   Classification.new(0)
+   Classification.new(TAX_EXEMPT_RATE)
   end
 
   def self.MEDICINE
-   Classification.new(0)
+   Classification.new(TAX_EXEMPT_RATE)
   end
 
   def self.FOOD
-   Classification.new(0)
+   Classification.new(TAX_EXEMPT_RATE)
   end
 
   def self.OTHER
-   Classification.new(10)
+   Classification.new(DEFAULT_TAX_RATE)
   end
 
-
   def self.IMPORTED(classification)
-   Classification.new(classification.tax_rate + 7)
+   Classification.new(classification.tax_rate + IMPORT_TAX_RATE)
   end
 
   private		#so that no one else can create any new classifications
