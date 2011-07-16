f6a80b6bf96cdc3aa1384eecfefa8db693f8d885
AU Zoso <changingrainbows@gmail.com>
2011-07-16 17:04:06 -0300

    fixed pending test (rounding)

--- spec/tax_spec.rb
+++ spec/tax_spec.rb
@@ -12,16 +12,15 @@ it "should calculate tax for a basic product" do
     tax = Tax.BASIC(Tax.EMPTY)
     price = 10
     basic_tax_rate = 0.1
-    tax.calculate_tax(price).should == price * basic_tax_rate
+    tax.calculate_tax(price).should == 1
   end
 
 it "should calculate tax for a basic, imported product" do
-    pending
     tax = Tax.BASIC(Tax.IMPORTED(Tax.EMPTY))
     price = 10
     basic_tax_rate = 0.1
     imported_tax_rate = 0.05
-    tax.calculate_tax(price).should == price * (basic_tax_rate + imported_tax_rate)
+    tax.calculate_tax(price).should == 1.5
   end
 
 end
