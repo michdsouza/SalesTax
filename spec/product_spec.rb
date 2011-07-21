a9b543b692c81f8d7ff6cedbabc137aeea8515b7
Michelle D'Souza <michd2005@gmail.com>
2011-07-21 20:15:14 -0300

    fixed product spec tests

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -4,35 +4,35 @@ require 'tax.rb'
 describe Product do
 
 it "should create a new product with no tax" do
-    blanket = Product.new("blanket", 10, Tax.EMPTY)
+    blanket = Product.new("blanket", 10, Tax.Applicable_Taxes([]))
     blanket.calculate_tax.should == 0
     blanket.price_with_tax.should == 10
   end
 
   it "should calculate basic tax for a product" do
-    blanket = Product.new("blanket", 10, Tax.BASIC(Tax.EMPTY))
+    blanket = Product.new("blanket", 10, Tax.Applicable_Taxes([10]))
     blanket.calculate_tax.should == 1
   end
 
   it "should calculate price (with tax) for an exempt product" do
-    pills = Product.new("pills", 19.95, Tax.EXEMPT(Tax.EMPTY))
+    pills = Product.new("pills", 19.95, Tax.Applicable_Taxes([0]))
     pills.price_with_tax.should == 19.95
   end
 
  it "should calculate price (with tax) for an imported product" do
-    fancy_blanket = Product.new("fancy blanket", 19.95, Tax.IMPORTED(Tax.BASIC(Tax.EMPTY)))
+    fancy_blanket = Product.new("fancy blanket", 19.95, Tax.Applicable_Taxes([10, 5]))
     fancy_blanket.calculate_tax.should == 3.0
     fancy_blanket.price_with_tax.should == 22.95
   end
 
  it "should calculate price (with tax) for a product regardless of taxation order" do
-    fancy_blanket = Product.new("fancy blanket", 19.95, Tax.BASIC(Tax.IMPORTED(Tax.EMPTY)))
+    fancy_blanket = Product.new("fancy blanket", 19.95, Tax.Applicable_Taxes([5, 10]))
     fancy_blanket.calculate_tax.should == 3.0
     fancy_blanket.price_with_tax.should == 22.95
   end
 
  it "should calculate price (with tax) for an imported, exempt product" do
-    fancy_chocolates = Product.new("box of imported chocolates", 11.25, Tax.IMPORTED(Tax.EXEMPT(Tax.EMPTY)))
+    fancy_chocolates = Product.new("box of imported chocolates", 11.25, Tax.Applicable_Taxes([5, 0]))
     fancy_chocolates.calculate_tax.should == 0.6 #still need this...for tax summation
     fancy_chocolates.price_with_tax.should == 11.85
   end
