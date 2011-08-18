67e8ab645c651952998ebc314d3c03c0dd7d8a39
AU Zoso <changingrainbows@gmail.com>
2011-08-17 19:55:07 -0600

    more spec shrinkage

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -3,51 +3,50 @@ require 'tax.rb'
 
 describe Product do
 
+ before (:all) do
+    @tax = Tax.EMPTY
+    @imported_tax = Tax.new(@tax, 5)
+    @basic_tax = Tax.new(@tax, 10)
+    @exempt_tax = Tax.new(@tax, 0)
+    BASIC_RATE = 10
+    IMPORTED_RATE = 5
+    EXEMPT_RATE = 0
+  end
+
 it "should create a new product with no tax" do
-    tax = Tax.EMPTY
-    blanket = Product.new("blanket", 10, tax)
+    blanket = Product.new("blanket", 10, @tax)
     blanket.calculate_tax.should == 0
     blanket.price_with_tax.should == 10
   end
 
   it "should calculate basic tax for a product" do
-    tax = Tax.EMPTY
-    tax = Tax.new(tax, 10)
-    blanket = Product.new("blanket", 10, tax)
+    blanket = Product.new("blanket", 10, @basic_tax)
     blanket.calculate_tax.should == 1
   end
 
   it "should calculate price (with tax) for an exempt product" do
-    tax = Tax.EMPTY
-    tax = Tax.new(tax, 0)
-    pills = Product.new("pills", 19.95, tax)
+    pills = Product.new("pills", 19.95, @exempt_tax)
     pills.price_with_tax.should == 19.95
   end
 
  it "should calculate price (with tax) for an imported product" do
-    tax = Tax.EMPTY
-    tax = Tax.new(tax, 10)
-    tax = Tax.new(tax, 5)
+    tax = Tax.new(@basic_tax, IMPORTED_RATE)
     fancy_blanket = Product.new("fancy blanket", 19.95, tax)
     fancy_blanket.calculate_tax.should == 3.0
     fancy_blanket.price_with_tax.should == 22.95
   end
 
  it "should calculate price (with tax) for a product regardless of taxation order" do
-    tax = Tax.EMPTY
-    tax = Tax.new(tax, 5)
-    tax = Tax.new(tax, 10)
+    tax = Tax.new(@imported_tax, BASIC_RATE)
     fancy_blanket = Product.new("fancy blanket", 19.95, tax)
     fancy_blanket.calculate_tax.should == 3.0
     fancy_blanket.price_with_tax.should == 22.95
   end
 
  it "should calculate price (with tax) for an imported, exempt product" do
-    tax = Tax.EMPTY
-    tax = Tax.new(tax, 5)
-    tax = Tax.new(tax, 0)
+    tax = Tax.new(@imported_tax, EXEMPT_RATE)
     fancy_chocolates = Product.new("box of imported chocolates", 11.25, tax)
-    fancy_chocolates.calculate_tax.should == 0.6   #used in tax summation
+    fancy_chocolates.calculate_tax.should == 0.6
     fancy_chocolates.price_with_tax.should == 11.85
   end
 
--- spec/tax_spec.rb
+++ spec/tax_spec.rb
@@ -2,25 +2,25 @@ require 'tax.rb'
 
 describe Tax do
 
+  before (:all) do
+    @tax = Tax.EMPTY
+    @price = 10
+    @imported_tax = Tax.new(@tax, 5)
+    @basic_tax = Tax.new(@tax, 10)
+    @exempt_tax = Tax.new(@tax, 0)
+  end
+
 it "should add no tax for an unspecified tax type" do
-    tax = Tax.EMPTY
-    price = 10
-    tax.calculate_tax(price).should == 0
+    @tax.calculate_tax(@price).should == 0
   end
 
 it "should calculate tax for a basic product" do
-    tax = Tax.EMPTY
-    tax = Tax.new(tax, 10)
-    price = 10
-    tax.calculate_tax(price).should == 1
+    @basic_tax.calculate_tax(@price).should == 1
   end
 
 it "should calculate tax for a basic, imported product" do
-    tax = Tax.EMPTY
-    tax = Tax.new(tax, 10)
-    tax = Tax.new(tax, 5)
-    price = 10
-    tax.calculate_tax(price).should == 1.5
+    tax = Tax.new(@basic_tax, 5)
+    tax.calculate_tax(@price).should == 1.5
   end
 
 end
